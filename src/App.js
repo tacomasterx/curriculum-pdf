b5bd9b8973e725001d8d3ca68feec89dca3f4c05
Tomas Carrillo <tacoman8918@gmail.com>
2022-08-09 15:07:12 -0500

    PDF working, starting better form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import {Button} from 'react-bootstrap';
+import {PDFViewer, PDFDownloadLink} from '@react-pdf/renderer';
 // Componente definido para almacenar el formulario
 import Formulario from './components/Formulario';
 // Componente definido para almacenar la previsualización web
@@ -24,6 +25,10 @@ function App() {
         <div className="View-container">
           <div className="View">
             {verWeb ? <VistaWeb campos={campos} /> : null}
+            {verPdf ?
+              <PDFViewer style={{width: "100%", height: "90vh"}}>
+                <VistaPdf campos={campos} />
+              </PDFViewer> : null}
           </div>
           <Button onClick={() => {
             setVerWeb(!verWeb);
@@ -33,7 +38,7 @@ function App() {
             setVerPdf(!verPdf);
             setVerWeb(false);
           }}>{verPdf ? 'Ocultar Pdf' : 'Ver Pdf'}</Button>
-          <Button>Descarga</Button>
+          <PDFDownloadLink document={<VistaPdf campos={campos} />} fileName={`Curriculum_Vitae - ${campos.campo}.pdf`}><Button>Descarga</Button></PDFDownloadLink>
         </div>
       </div>
     </div >
--- src/components/VistaPdf.js
+++ src/components/VistaPdf.js
@@ -1,17 +1,18 @@
-// import {Document, Page, Text} from '@react-pdf/renderer';
-// const VistaPdf = ({campos}) => {
-//   return (
-//     <Document>
-//       <Page size="A4">
-//         <Text>
-//           {campos.campo != null ?
-//             campos.campo :
-//             'No se han ingresado datos.'}
-//         </Text>
-//       </Page>
-//     </Document>
-//   );
-// }
+import {Document, Page, Text} from '@react-pdf/renderer';
 
-// export default VistaPdf;
+const VistaPdf = ({campos}) => {
+  return (
+    <Document>
+      <Page size="A4">
+        <Text>
+          {campos.campo != null ?
+            campos.campo :
+            'No se han ingresado datos.'}
+        </Text>
+      </Page>
+    </Document>
+  );
+}
+
+export default VistaPdf;
 
